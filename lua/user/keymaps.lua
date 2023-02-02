c7b2110eb96a28d0dbc595f0efe3fb7c9a56ce7b
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2023-02-02 13:04:32 -0800

    Another mass change commit. I'll make smaller commits in the future.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -14,37 +14,42 @@ local keymap = vim.keymap.set
 --   command_mode = "c",
 
 --Remap space as leader key
-keymap("n", "<Space>", "<Nop>", opts)
+keymap("n", "<Space>", "<NOP>", opts)  -- TODO: Doesn't work for some reason
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Disable Backspace key in normal mode
--- keymap("n", "<BS>", "<nop>", opts)
+keymap("n", "<BS>", "<nop>", opts)
 
 -- Move the Redo keymap to a more sensible mapping
 keymap("n", "R", "<Nop>", opts)
 keymap("n", "U", ":redo<CR>", opts)
 
 -- Double tap ESC to hide text highlighting, from search for example
-keymap("n", "<ESC><ESC>", "<cmd>noh<CR>", opts)
-
-keymap("n", "J", "<Nop>", opts)
+keymap("n", "<ESC><ESC>", ":noh<CR>", opts)
 
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<S-h>", "<C-w>h", opts)  -- Move cursor to window left
+keymap("n", "<S-j>", "<C-w>j", opts)  -- Move cursor to window up
+keymap("n", "<S-k>", "<C-w>k", opts)  -- Move cursor to window down
+keymap("n", "<S-l>", "<C-w>l", opts)  -- Move cursor to window right
+
+-- Keymaps for if you have holding capslock be ctrl and tapping capslock be ESC
+keymap("n", "<c-k>", ":bnext<cr>", opts)
+keymap("n", "<c-j>", ":bprevious<cr>", opts)
+keymap('n', '<c-h>', '<c-o>',opts)  -- Go Back
+keymap('n', '<c-l>', '<c-i>',opts)  -- Go Forward
+
+-- I don't use a keyboard with a 10-key so these keymaps aren't useful to me.
+keymap("n", "-", "<nop>", opts)
+keymap("n", "+", "<nop>", opts)
 
 -- Resize with arrows
 keymap("n", "<A-Up>", ":resize -2<CR>", opts)
 keymap("n", "<A-Down>", ":resize +2<CR>", opts)
 keymap("n", "<A-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<A-Right>", ":vertical resize +2<CR>", opts)
-
--- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Move text up and down
 keymap("n", "<A-j>", ":move .+1<CR>==", opts)
@@ -53,9 +58,7 @@ keymap("n", "<A-k>", ":move .-2<CR>==", opts)
 keymap("v", "<A-k>", ":move .-2<CR>==", opts)
 keymap("i", "<A-j>", "<ESC>:move .+1<CR>==i", opts)
 keymap("i", "<A-k>", "<ESC>:move .-2<CR>==i", opts)
-keymap("x", "J",     ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K",     ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Helix style keymaps
@@ -91,5 +94,5 @@ keymap("n", "<F5>", ":UndotreeToggle<cr>:UndotreeFocus<cr>", opts)
 
 if vim.fn.exists("vim.g.plugs.undotree") then
     vim.g.loaded_undotree = 2
-    vim.api.nvim_set_keymap("n", "<Leader>u", "<Cmd>UndotreeToggle<CR>", {noremap = true})
+    vim.api.nvim_set_keymap("n", "<Leader>u", ":UndotreeToggle<CR>", {noremap = true})
 end
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -171,9 +171,9 @@ return packer.startup(function(use)
                     -- modify status buffer mappings
                     status = {
                         -- Adds a mapping with "B" as key that does the "BranchPopup" command
-                        ["B"] = "BranchPopup",
+                        -- ["B"] = "BranchPopup",
                         -- Removes the default mapping of "s"
-                        ["s"] = "",
+                        -- ["s"] = "",
                     }
                 }
             }
