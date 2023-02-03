9d43149308704dee0a2381d1d34e94f43e994393
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2023-02-02 23:33:54 -0800

    Add nvim-ordmode plugin and added full URL for each repo to make them to make them easier to click on to read up on documentation later.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -40,59 +40,94 @@ packer.init({
 
 -- Install your plugins here
 return packer.startup(function(use)
-    use { "mbbill/undotree", commit = "1a23ea84bd02c34f50d8e10a8b4bfc89597ffe4e" } -- Like git commits but for undos/redos
-    use { "christoomey/vim-titlecase", commit = "69a697c54a08ca5603cb757b4d4f78cd4b567549" }
-    use { "rmagatti/auto-session", commit = "c8b2f4048f846387361bd04cc185bf1aa7d2e3d1" }
-    use { "wbthomason/packer.nvim", commit = "6afb67460283f0e990d35d229fd38fdc04063e0a" } -- Have packer manage itself
-    use { "nvim-lua/plenary.nvim", commit = "4b7e52044bbb84242158d977a50c4cbcd85070c7" } -- Useful lua functions used by lots of plugins
-    use { "windwp/nvim-autopairs", commit = "4fc96c8f3df89b6d23e5092d31c866c53a346347" } -- Autopairs, integrates with both cmp and treesitter
-    use { "numToStr/Comment.nvim", commit = "97a188a98b5a3a6f9b1b850799ac078faa17ab67" }
-    use { "JoosepAlviste/nvim-ts-context-commentstring", commit = "4d3a68c41a53add8804f471fcc49bb398fe8de08" }
-    use { "kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" }
-    use { "kyazdani42/nvim-tree.lua", commit = "7282f7de8aedf861fe0162a559fc2b214383c51c" }
-    use { "akinsho/bufferline.nvim", commit = "83bf4dc7bff642e145c8b4547aa596803a8b4dc4" }
-    use { "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" }
-    use { "nvim-lualine/lualine.nvim", commit = "a52f078026b27694d2290e34efa61a6e4a690621" }
-    use { "akinsho/toggleterm.nvim", commit = "2a787c426ef00cb3488c11b14f5dcf892bbd0bda" }
-    use { "ahmedkhalf/project.nvim", commit = "628de7e433dd503e782831fe150bb750e56e55d6" }
-    use { "lewis6991/impatient.nvim", commit = "b842e16ecc1a700f62adb9802f8355b99b52a5a6" }
-    use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
-    use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
-    use {"folke/which-key.nvim"}
+    use { 'https://github.com/nvim-orgmode/orgmode',
+        requires = 'https://github.com/nvim-treesitter/nvim-treesitter',
+        config = function()
+            require('orgmode').setup{
+                -- Load custom treesitter grammar for org filetype
+                require('orgmode').setup_ts_grammar(),
+
+                -- Treesitter configuration
+                require('nvim-treesitter.configs').setup {
+                    -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+                    -- highlighting will fallback to default Vim syntax highlighting
+                    highlight = {
+                        enable = true,
+                        -- Required for spellcheck, some LaTex highlights and
+                        -- code block highlights that do not have ts grammar
+                        additional_vim_regex_highlighting = {'org'},
+                    },
+                    ensure_installed = {'org'}, -- Or run :TSUpdate org
+                },
+
+                require('orgmode').setup({
+                    org_agenda_files = {'~/Dropbox/org/*', '~/my-orgs/**/*'},
+                    org_default_notes_file = '~/Dropbox/org/refile.org',
+                })
+            }
+        end
+    }
+    use { 'https://github.com/nvim-treesitter/nvim-treesitter' }
+    -- Integrates with MacOS to automatically switch to Light or Dark mode.
+    use { 'https://github.com/will/bgwinch.nvim',
+        config = function()
+            require("bgwinch").setup()
+        end,
+    }
+    use { "mbbill/undotree", commit = "1a23ea84bd02c34f50d8e10a8b4bfc89597ffe4e",
+        config = function()
+            vim.g.undotree_WindowLayout = 2
+        end
+    }
+    use { "https://github.com/christoomey/vim-titlecase", commit = "69a697c54a08ca5603cb757b4d4f78cd4b567549" }
+    use { "https://github.com/rmagatti/auto-session", commit = "c8b2f4048f846387361bd04cc185bf1aa7d2e3d1" }
+    use { "https://github.com/wbthomason/packer.nvim", commit = "6afb67460283f0e990d35d229fd38fdc04063e0a" } -- Have packer manage itself
+    use { "https://github.com/nvim-lua/plenary.nvim", commit = "4b7e52044bbb84242158d977a50c4cbcd85070c7" } -- Useful lua functions used by lots of plugins
+    use { "https://github.com/windwp/nvim-autopairs", commit = "4fc96c8f3df89b6d23e5092d31c866c53a346347" } -- Autopairs, integrates with both cmp and treesitter
+    use { "https://github.com/numToStr/Comment.nvim", commit = "97a188a98b5a3a6f9b1b850799ac078faa17ab67" }
+    use { "https://github.com/JoosepAlviste/nvim-ts-context-commentstring", commit = "4d3a68c41a53add8804f471fcc49bb398fe8de08" }
+    use { "https://github.com/kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" }
+    use { "https://github.com/kyazdani42/nvim-tree.lua", commit = "7282f7de8aedf861fe0162a559fc2b214383c51c" }
+    use { "https://github.com/akinsho/bufferline.nvim", commit = "83bf4dc7bff642e145c8b4547aa596803a8b4dc4" }
+    use { "https://github.com/moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" }
+    use { "https://github.com/nvim-lualine/lualine.nvim", commit = "a52f078026b27694d2290e34efa61a6e4a690621" }
+    use { "https://github.com/akinsho/toggleterm.nvim", commit = "2a787c426ef00cb3488c11b14f5dcf892bbd0bda" }
+    use { "https://github.com/ahmedkhalf/project.nvim", commit = "628de7e433dd503e782831fe150bb750e56e55d6" }
+    use { "https://github.com/lewis6991/impatient.nvim", commit = "b842e16ecc1a700f62adb9802f8355b99b52a5a6" }
+    use { "https://github.com/lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
+    use { "https://github.com/goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
+    use {"https://github.com/folke/which-key.nvim"}
 
     -- Colorschemes
-    use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
+    use { "https://github.com/folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
     use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }
-    use { "ishan9299/nvim-solarized-lua" }
+    use { "https://github.com/ishan9299/nvim-solarized-lua" }
 
     -- Cmp 
-    use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
-    use { "hrsh7th/cmp-buffer", commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa" } -- buffer completions
-    use { "hrsh7th/cmp-path", commit = "447c87cdd6e6d6a1d2488b1d43108bfa217f56e1" } -- path completions
-    use { "saadparwaiz1/cmp_luasnip", commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36" } -- snippet completions
-    use { "hrsh7th/cmp-nvim-lsp", commit = "3cf38d9c957e95c397b66f91967758b31be4abe6" }
-    use { "hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" }
+    use { "https://github.com/hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
+    use { "https://github.com/hrsh7th/cmp-buffer", commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa" } -- buffer completions
+    use { "https://github.com/hrsh7th/cmp-path", commit = "447c87cdd6e6d6a1d2488b1d43108bfa217f56e1" } -- path completions
+    use { "https://github.com/saadparwaiz1/cmp_luasnip", commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36" } -- snippet completions
+    use { "https://github.com/hrsh7th/cmp-nvim-lsp", commit = "3cf38d9c957e95c397b66f91967758b31be4abe6" }
+    use { "https://github.com/hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" }
 
     -- Snippets
-    use { "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" } --snippet engine
-    use { "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" } -- a bunch of snippets to use
+    use { "https://github.com/L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" } --snippet engine
+    use { "https://github.com/rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" } -- a bunch of snippets to use
 
     -- LSP
-    use { "neovim/nvim-lspconfig", commit = "f11fdff7e8b5b415e5ef1837bdcdd37ea6764dda" } -- enable LSP
-    use { "williamboman/mason.nvim", commit = "c2002d7a6b5a72ba02388548cfaf420b864fbc12"} -- simple to use language server installer
-    use { "williamboman/mason-lspconfig.nvim", commit = "0051870dd728f4988110a1b2d47f4a4510213e31" }
-    use { "jose-elias-alvarez/null-ls.nvim", commit = "c0c19f32b614b3921e17886c541c13a72748d450" } -- for formatters and linters
-    use { "RRethy/vim-illuminate", commit = "a2e8476af3f3e993bb0d6477438aad3096512e42" }
+    use { "https://github.com/neovim/nvim-lspconfig", commit = "f11fdff7e8b5b415e5ef1837bdcdd37ea6764dda" } -- enable LSP
+    use { "https://github.com/williamboman/mason.nvim", commit = "c2002d7a6b5a72ba02388548cfaf420b864fbc12"} -- simple to use language server installer
+    use { "https://github.com/williamboman/mason-lspconfig.nvim", commit = "0051870dd728f4988110a1b2d47f4a4510213e31" }
+    use { "https://github.com/jose-elias-alvarez/null-ls.nvim", commit = "c0c19f32b614b3921e17886c541c13a72748d450" } -- for formatters and linters
+    use { "https://github.com/RRethy/vim-illuminate", commit = "a2e8476af3f3e993bb0d6477438aad3096512e42" }
 
     -- Telescope
-    use { "nvim-telescope/telescope.nvim", commit = "76ea9a898d3307244dce3573392dcf2cc38f340f" }
-
-    -- Treesitter
-    use { "nvim-treesitter/nvim-treesitter", commit = "8e763332b7bf7b3a426fd8707b7f5aa85823a5ac" }
+    use { "https://github.com/nvim-telescope/telescope.nvim", commit = "76ea9a898d3307244dce3573392dcf2cc38f340f" }
 
     -- Git
-    use { "lewis6991/gitsigns.nvim", commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2" }
-    use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim',
+    use { "https://github.com/lewis6991/gitsigns.nvim", commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2" }
+    use { 'https://github.com/TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim',
         config = function ()
             local neogit = require("neogit")
 
