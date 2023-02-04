02cd86f8e3123806eb462149ea4b775b52d25fda
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2023-02-03 19:13:58 -0800

    Fix orgmode config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -41,33 +41,56 @@ packer.init({
 -- Install your plugins here
 return packer.startup(function(use)
     use { 'https://github.com/nvim-orgmode/orgmode',
-        requires = 'https://github.com/nvim-treesitter/nvim-treesitter',
+        -- requires = 'https://github.com/nvim-treesitter/nvim-treesitter',
         config = function()
-            require('orgmode').setup{
-                -- Load custom treesitter grammar for org filetype
-                require('orgmode').setup_ts_grammar(),
+            -- Load custom treesitter grammar for org filetype
+            require('orgmode').setup_ts_grammar()
 
-                -- Treesitter configuration
-                require('nvim-treesitter.configs').setup {
-                    -- If TS highlights are not enabled at all, or disabled via `disable` prop,
-                    -- highlighting will fallback to default Vim syntax highlighting
-                    highlight = {
-                        enable = true,
-                        -- Required for spellcheck, some LaTex highlights and
-                        -- code block highlights that do not have ts grammar
-                        additional_vim_regex_highlighting = {'org'},
-                    },
-                    ensure_installed = {'org'}, -- Or run :TSUpdate org
+            -- Treesitter configuration
+            require('nvim-treesitter.configs').setup {
+                -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+                -- highlighting will fallback to default Vim syntax highlighting
+                highlight = {
+                    enable = true,
+                    -- Required for spellcheck, some LaTex highlights and
+                    -- code block highlights that do not have ts grammar
+                    additional_vim_regex_highlighting = {'org'},
                 },
-
-                require('orgmode').setup({
-                    org_agenda_files = {'~/Dropbox/org/*', '~/my-orgs/**/*'},
-                    org_default_notes_file = '~/Dropbox/org/refile.org',
-                })
+                ensure_installed = {'org'}, -- Or run :TSUpdate org
             }
+
+            require('orgmode').setup({
+                org_agenda_files = {'~/Dropbox/org/*', '~/my-orgs/**/*'},
+                org_default_notes_file = '~/Dropbox/org/refile.org',
+            })
+
+            -- PICK ONE COMPLETION OPTION --
+
+            -- Option 1 nvim-compe
+            -- require('compe').setup({
+            --     source = {
+            --         orgmode = true
+            --     })
+            -- Option 2 nvim-cmp
+            require('cmp').setup({
+                sources = {
+                    { name = 'orgmode' }
+                }
+            })
+            -- Option 3 completion-nvim
+            -- vim.g.completion_chain_complete_list = {
+            --     org = {
+            --         { mode = 'omni'},
+            --     },
+            -- }
+            -- -- add additional keyword chars
+            -- vim.cmd[[autocmd FileType org setlocal iskeyword+=:,#,+]]
+
+            -- END OF COMPLETION OPTIONS --
         end
     }
     use { 'https://github.com/nvim-treesitter/nvim-treesitter' }
+
     -- Integrates with MacOS to automatically switch to Light or Dark mode.
     use { 'https://github.com/will/bgwinch.nvim',
         config = function()
